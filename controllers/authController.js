f95a5351435fe7e77961cf566630df4df762e6e0
Mahmoud Nagy <nagymedo25@gmail.com>
2025-09-15 00:23:23 +0300

    fix(auth): Use SameSite='None' for cross-domain cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const sendTokenCookie = (res, token) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none~",
+    sameSite: "none",
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
   };
   res.cookie("token", token, cookieOptions);
